1f9d6f8fb874b1e74b1d7538d52250801a9f04b1
kelvin <kellai@gmail.com>
2022-11-20 15:08:18 -0800

    compile contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
 contract Inbox {
-    string public message; //exists inside my contract
-
-    constructor (string initialMessage) public {
+    string public message;
+    
+    function Inbox(string initialMessage) public {
         message = initialMessage;
     }
+    
     function setMessage(string newMessage) public {
         message = newMessage;
     }
