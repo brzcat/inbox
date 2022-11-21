ee2550bf8ad71e8a9cc4c7555c26cd46e276b7f5
kelvin <kellai@gmail.com>
2022-11-20 17:20:11 -0800

    deploy contract to goerli network

--- compile.js
+++ compile.js
@@ -7,5 +7,5 @@ const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
 //console.log(solc.compile(source, 1).contracts[':Inbox']);
-
+//[<can_be_file_name>:Inbox]
 module.exports = solc.compile(source,1).contracts[':Inbox'];
